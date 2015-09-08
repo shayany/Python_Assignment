b995392ea34a78eac3c5c6e7247bf6583d433020
Shayan Yazdanmehr <shayany@ifi.uio.no>
2015-09-08 14:42:43 +0200

    simplestring.py has been created

---
+++ week3/simplestring.py
@@ -0,0 +1,18 @@
+class SimpleString(object):
+    """
+    This class has two functions :
+    1-getString: to get a string from console input
+    2-printString: to print the string in upper case
+    """
+    def getString(self):
+        """
+        This function get string value from a terminal 
+        and save that value in a local variable which called
+        userInput
+        """
+        self.userInput = raw_input("Enter string value:")
+    def printString(self):
+        """
+        Print value of the self.userInput on terminal
+        """
+        print self.userInput
