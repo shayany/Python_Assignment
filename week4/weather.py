7b075675961b5f21536fc68b11273f8e07a31074
Shayan Yazdanmehr <shayany@ifi.uio.no>
2015-09-21 16:38:39 +0200

    Find link to location has been implemented

--- week4/weather.py
+++ week4/weather.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# encoding: utf-8
 from subprocess import Popen,PIPE
 import re
 import urllib
@@ -6,8 +8,9 @@ def getContent(url):
     """
     Return contents of the URL.
     """
-    return urllib.urlopen(url).read()
-    #return Popen("curl "+url,stdout=PIPE).communicate()[0]
+    return str(urllib.urlopen(url).read())
+    #stringmy =Popen("curl http://fil.nrk.no/yr/viktigestader/noreg.txt",stdout=PIPE).communicate()[0]
+    #print stringmy
 
 def findLink(name):
     """
@@ -24,12 +27,39 @@ def findLink(name):
     else Return EMPTY LIST
     
     """
-    plainTextFile=getContent(u"http://fil.nrk.no/yr/viktigestader/noreg.txt");
-    noregTable=plainTextFile.split("\r\n");
-    xmlList=[]
-    #reTemplate=re.template(name)
+    plainTextFile=getContent("http://fil.nrk.no/yr/viktigestader/noreg.txt") #Get content from this URL
+    noregTable=plainTextFile.split("\r\n") #Split the lines into the list 
+    xmlList=[]#Create Empty list to return results 
+    name=name.replace("?",u".?")    #Replace wildcards (this is the format that regex support) 
+    name=name.replace("*",u".*")    #Replace wildcards (this is the format that regex support) 
+    
     for counter in range(len(noregTable)-1):
-        noregTable[counter]=noregTable[counter].split("\t")
-        if re.findall(r"^\bSv.*\b",noregTable[counter][1]):
-            xmlList.append(noregTable[counter][1])
-    print xmlList
+        noregTable[counter]=noregTable[counter].split("\t") #Split each line to a list of fields 
+        if re.findall(r"(?i)^{0}$".format(name.encode("utf-8")),str(noregTable[counter][1]).decode("utf-8")):
+            if not str(noregTable[counter][13]).decode("utf-8") in xmlList: 
+                xmlList.append(str(noregTable[counter][13]).decode("utf-8"))                      
+    if not xmlList:
+        for counter in range(len(noregTable)-1):
+            if re.findall(r"(?i)^{0}$".format(name.encode("utf-8")),str(noregTable[counter][6]).decode("utf-8")):
+                if not str(noregTable[counter][13]).decode("utf-8") in xmlList: 
+                    xmlList.append(str(noregTable[counter][13]).decode("utf-8"))                    
+        if xmlList:
+            return xmlList
+    if not xmlList:
+        for counter in range(len(noregTable)-1):
+            if re.findall(r"(?i)^{0}$".format(name.encode("utf-8")),str(noregTable[counter][7]).decode("utf-8")):
+                if not str(noregTable[counter][13]).decode("utf-8") in xmlList: 
+                    xmlList.append(str(noregTable[counter][13]).decode("utf-8"))               
+        if xmlList:
+            return xmlList
+    if name=="":
+        for counter in range(len(noregTable)-1):
+            if not str(noregTable[counter][13]).decode("utf-8") in xmlList: 
+                xmlList.append(str(noregTable[counter][13]).decode("utf-8"))               
+        if xmlList:
+            return xmlList
+    return xmlList 
+
+#Query example       
+for item in findLink(""):
+    print item.encode("utf-8")
