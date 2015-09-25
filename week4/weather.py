0b612066600f49ed53510c70a9fc07734e1caddb
Shayan Yazdanmehr <shayany@ifi.uio.no>
2015-09-25 08:28:17 +0200

    Retrieving weather information

--- week4/weather.py
+++ week4/weather.py
@@ -27,15 +27,17 @@ def findLink(name=""):
     else Return EMPTY LIST
     
     """
+    if not(str.isalpha(name) or "*" in name or "?" in name or not name or " " in name):
+        raise  ValueError,"Enter only letters or wildcards(* or ?)"
     plainTextFile=getContent("http://fil.nrk.no/yr/viktigestader/noreg.txt") #Get content from this URL
     xmlList=[]#Create Empty list to return results 
     cityName=name.decode("utf-8").replace("?","[^\\t]?")    #Replace wildcards (this is the format that regex support) 
     cityName=cityName.replace("*","[^\\t]*?")    #Replace wildcards (this is the format that regex support) 
-    """
+
     if name=="":    
         xmlList = re.findall("^.*\\t(.*?)\\r$".format(cityName),plainTextFile,re.I+re.MULTILINE)
-        return list(set(xmlList))               
-    """
+        return list(set(xmlList))[1:] #removing the first line from the file(Titles)
+
     xmlList = re.findall(u"^\\d*\\t{0}\\t.*\\t(.*?)\\r$".format(cityName),plainTextFile,re.I+re.MULTILINE)
     if xmlList:
         return list(set(xmlList))   
@@ -45,23 +47,30 @@ def findLink(name=""):
         if xmlList:
             list(set(xmlList))    
     if not xmlList:
-        xmlList = re.findall(u"^\\d*\\t.*?\\t.*?\\t.*?\\t.*?\\t.*?\\t.*?\\t{0}\\t.*\\t(.*?)\\r$".format(cityName),plainTextFile,re.I+re.MULTILINE)
+        xmlList = re.findall(u"^\\d*\\t(?:.*?\\t){6}%s\\t.*\\t(.*?)\\r$"%cityName,plainTextFile,re.I+re.MULTILINE)
         if xmlList:
             list(set(xmlList))   
     return list(set(xmlList))
 
 def weatherInformation(url):
     content=getContent(url)
+    locationName=re.findall(u"<[^.]*?name[^.]*?>(.*?)<[^.]*?/[^.]*name[^.]*?>",content,re.I+re.MULTILINE)
+#    <[^.]*?name[^.]*?>(.*?)<[^.]*?/[^.]*name[^.]*?>
+    weatherContent=re.findall(u"<tabular>(.*?)</tabular>",content,re.I+re.MULTILINE+re.DOTALL)[0]
+    timeContent=re.findall((u"<time from=\"(.*?)\" to=\"(.*?)\" period=\"(?:.*?)\">"
+                            ".*?<symbol.*?name=\"(.*?)\".*?>.*?"
+                            "<precipitation value=\"(.*?)\".*?>.*?"
+                            "<windSpeed mps=\"(.*?)\".*?>.*?"
+                            "<temperature unit=\"(?:.*?)\" value=\"(.*?)\".*?>")
+                            ,weatherContent,re.I+re.MULTILINE+re.DOTALL)
+#    timeContent=re.findall(u"<time from=\"(.*?)\" to=\"(.*?)\" period=\"(?:.*?)\">.*?<symbol.*?name=\"(.*?)\".*?>.*?<precipitation value=\"(.*?)\".*?>.*?<windSpeed mps=\"(.*?)\".*?>.*?<temperature unit=\"(?:.*?)\" value=\"(.*?)\".*?>",weatherContent[0],re.I+re.MULTILINE+re.DOTALL)
+    return locationName,timeContent
+    #time=re.findall(u"(<time.*?/time>)",weathercontent,re.I+re.MULTILINE+re.DOTALL)
     #print type(content)
     #print content.encode("utf-8")
-    tabular=re.findall(u"<tabular>((\\s|\\r|\\n|.)*?)</tabular>",content.encode("utf-8"),re.I+re.MULTILINE)
-    print type(str(tabular[0]))
+    #tabular=re.findall(u"<tabular>((\\s|\\r|\\n|.)*?)</tabular>",content.encode("utf-8"),re.I+re.MULTILINE)
+    #print type(str(tabular[0]))
 #Query example       
-for item in findLink("Vestfold"):
-    print item.encode("utf-8")
-
-
-
-#weatherInformation(findLink("?s?land?")[0].encode("utf-8"))
-
-
+#for item in findLink("?s*land?"):
+#    print item.encode("utf-8")
+print weatherInformation(findLink("nordsinni")[0].encode("utf-8"))
