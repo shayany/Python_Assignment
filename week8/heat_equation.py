557a40bcac5dee6de4a02afec2cc12fd7400ac61
Shayan Yazdanmehr <shayany@ifi.uio.no>
2015-10-16 19:01:21 +0200

    Timer has been removed from Solver() and Solver_numpy()

--- week8/heat_equation.py
+++ week8/heat_equation.py
@@ -8,15 +8,15 @@ def solver(n=50,m=100,u=0,t0=0,t1=1000,dt=0.1,nu=1):
     pyplot.subplot(1,2,1)
     pyplot.imshow(u)
     loopCounter=t0
-    tt1=time.time()
+    #tt1=time.time()
     while(loopCounter<t1):
         for i in range(1,m-1):
             for j in range(1,n-1):
                 u[i][j]=u[i][j] + dt * (nu*u[i-1][j] + nu*u[i][j-1] - 4*nu*u[i][j] + nu*u[i][j+1] + nu*u[i+1][j] + f[i][j])
         loopCounter+=0.1
     #TIME
-    tt2=time.time()
-    print tt2-tt1
+    #tt2=time.time()
+    #print tt2-tt1
     #TIME    
     pyplot.subplot(1,2,2)
     pyplot.imshow(u)
--- week8/heat_equation_numpy.py
+++ week8/heat_equation_numpy.py
@@ -8,13 +8,13 @@ def solver_numpy(n=50,m=100,u=0,t0=0,t1=1000,dt=0.1,nu=1):
     pyplot.subplot(1,2,1)
     pyplot.imshow(u)
     loopCounter=t0
-    tt1=time.time()
+    #tt1=time.time()
     while(loopCounter<t1):
         u[1:m-1,1:n-1]=u[1:m-1,1:n-1] + dt * (nu*u[0:m-2,1:n-1] + nu*u[1:m-1,0:n-2] - 4*nu*u[1:m-1,1:n-1] + nu*u[1:m-1,2:n] + nu*u[2:m,1:n-1] + f[1:m-1,1:n-1]) 
         loopCounter+=0.1
     #TIME
-    tt2=time.time()
-    print tt2-tt1
+    #tt2=time.time()
+    #print tt2-tt1
     #TIME
     pyplot.subplot(1,2,2)
     pyplot.imshow(u)
