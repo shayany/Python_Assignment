43ce3edeb1edd8f58e7992b5eb2e9c6381bf66c3
Shayan Yazdanmehr <shayany@ifi.uio.no>
2015-10-20 15:29:40 +0200

    1-add copying function to 5.1  2-add dt to counter

--- week8/heat_equation.py
+++ week8/heat_equation.py
@@ -1,19 +1,22 @@
 from matplotlib import pyplot 
-from heat_equation_numpy import *
 import time 
-def solver(n=50,m=100,u=0,t0=0,t1=1000,dt=0.1,nu=1):
-
-    u=[[0 for x in range(n)]for x in range(m)]
-    f=[[1 for x in range(n)]for x in range(m)]
+import math
+def solver(n=50,m=100,t0=0,t1=1000,dt=.1,nu=1):
+    u=[[0.0 for x in range(n)]for x in range(m)]
+    f=[[1.0 for x in range(n)]for x in range(m)]
+    u_new=[[u[i][j] for j in range(n)]for i in range(m)]
     pyplot.subplot(1,2,1)
     pyplot.imshow(u)
     loopCounter=t0
+    #TIME
     #tt1=time.time()
+    #TIME
     while(loopCounter<t1):
-        for i in range(1,m-1):
-            for j in range(1,n-1):
-                u[i][j]=u[i][j] + dt * (nu*u[i-1][j] + nu*u[i][j-1] - 4*nu*u[i][j] + nu*u[i][j+1] + nu*u[i+1][j] + f[i][j])
-        loopCounter+=0.1
+        for i in xrange(1,m-1):
+            for j in xrange(1,n-1):
+                u_new[i][j]=u[i][j] + dt * (nu*u[i-1][j] + nu*u[i][j-1] - 4*nu*u[i][j] + nu*u[i][j+1] + nu*u[i+1][j] + f[i][j])
+        loopCounter+=dt
+        u=[[u_new[i][j] for j in range(n)]for i in range(m)]
     #TIME
     #tt2=time.time()
     #print tt2-tt1
@@ -22,4 +25,5 @@ def solver(n=50,m=100,u=0,t0=0,t1=1000,dt=0.1,nu=1):
     pyplot.imshow(u)
     pyplot.colorbar()
     pyplot.show()
-
+    return u
+    
--- week8/heat_equation_numpy.py
+++ week8/heat_equation_numpy.py
@@ -1,17 +1,30 @@
+# -*- coding: utf-8 -*-
 import time 
 from matplotlib import pyplot 
 from numpy import *
-def solver_numpy(n=50,m=100,u=0,t0=0,t1=1000,dt=0.1,nu=1):
+def solver_numpy(n=50,m=100,t0=0,t1=1000,dt=.1,nu=1):
     
-    u=zeros((m,n),dtype='float')
-    f=ones((m,n),dtype='float')
+    u=array(zeros((m,n),dtype='float'))
+    f=array(ones((m,n),dtype='float'))
     pyplot.subplot(1,2,1)
     pyplot.imshow(u)
     loopCounter=t0
+    #TIME
     #tt1=time.time()
+    #TIME
+    firstRow=1
+    firstCol=1
+    lastRow=m-1       
+    lastCol=n-1
     while(loopCounter<t1):
-        u[1:m-1,1:n-1]=u[1:m-1,1:n-1] + dt * (nu*u[0:m-2,1:n-1] + nu*u[1:m-1,0:n-2] - 4*nu*u[1:m-1,1:n-1] + nu*u[1:m-1,2:n] + nu*u[2:m,1:n-1] + f[1:m-1,1:n-1]) 
-        loopCounter+=0.1
+        u[firstRow:lastRow,firstCol:lastCol]=u[firstRow:lastRow,firstCol:lastCol] + dt \
+                                            * (nu*u[firstRow-1:lastRow-1,firstCol:lastCol] \
+                                            + nu*u[firstRow:lastRow,firstCol-1:lastCol-1] \
+                                            - 4*nu*u[firstRow:lastRow,firstCol:lastCol] \
+                                            + nu*u[firstRow:lastRow,firstCol+1:lastCol+1] \
+                                            + nu*u[firstRow+1:lastRow+1,firstCol:lastCol] \
+                                            + f[firstRow:lastRow,firstCol:lastCol])                 
+        loopCounter+=dt
     #TIME
     #tt2=time.time()
     #print tt2-tt1
@@ -20,3 +33,4 @@ def solver_numpy(n=50,m=100,u=0,t0=0,t1=1000,dt=0.1,nu=1):
     pyplot.imshow(u)
     pyplot.colorbar()
     pyplot.show()
+    return u
