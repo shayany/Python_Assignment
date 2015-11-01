b944b833f84990fd1ec1d961675999d20ec7ac87
Shayan Yazdanmehr <shayany@ifi.uio.no>
2015-11-01 18:16:11 +0100

    main commenting and readme

--- week8/Readme.md
+++ week8/Readme.md
@@ -1,1 +1,2 @@
 Follow instructions in SWIG and Cython directories :)
+You can start from the main and see the results.
--- week8/main.py
+++ week8/main.py
@@ -1,5 +1,5 @@
 """
-This file can be usefeul for debugging and timing purpose!
+In this file you can see the result for all of implementations!
 """
 from heat_equation import solver
 from heat_equation_instant import solver_instant
@@ -14,31 +14,31 @@ col=100
 u_list=[[0.0 for x in range(row)]for x in range(col)]
 f_list=[[1.0 for x in range(row)]for x in range(col)]
 
-#ShowPlot(u_list)
-%timeit plainPython_result=solver(u_list,f_list,col,row,t0=0,t1=1000,dt=.1,nu=1)
-#ShowPlot(plainPython_result)
+ShowPlot(u_list)
+plainPython_result=solver(u_list,f_list,col,row,t0=0,t1=1000,dt=.1,nu=1)
+ShowPlot(plainPython_result)
 
 
 u_numpy=array(zeros((col,row),dtype='double'))
 f_numpy=array(ones((col,row),dtype='double'))
-#ShowPlot(u_numpy)
-%timeit Cython_result=solver_cython(u_numpy,f_numpy,col,row,t0=0,t1=1000,dt=.1,nu=1)
-#ShowPlot(Cython_result)
+ShowPlot(u_numpy)
+Cython_result=solver_cython(u_numpy,f_numpy,col,row,t0=0,t1=1000,dt=.1,nu=1)
+ShowPlot(Cython_result)
 
 u_numpy=array(zeros((col,row),dtype='double'))
 f_numpy=array(ones((col,row),dtype='double'))
-#ShowPlot(u_numpy)
-%timeit Numpy_result=solver_numpy(u_numpy,f_numpy,col,row,t0=0,t1=1000,dt=.1,nu=1)
-#ShowPlot(Numpy_result)
+ShowPlot(u_numpy)
+Numpy_result=solver_numpy(u_numpy,f_numpy,col,row,t0=0,t1=1000,dt=.1,nu=1)
+ShowPlot(Numpy_result)
 
 u_numpy=array(zeros((col,row),dtype='double'))
 f_numpy=array(ones((col,row),dtype='double'))
-#ShowPlot(u_numpy)
-%timeit Swig_result=solver_swig(u_numpy,f_numpy,col,row,t0=0,t1=1000,dt=.1,nu=1)
-#ShowPlot(Swig_result)
+ShowPlot(u_numpy)
+Swig_result=solver_swig(u_numpy,f_numpy,col,row,t0=0,t1=1000,dt=.1,nu=1)
+ShowPlot(Swig_result)
 
 u_numpy=array(zeros((col,row),dtype='double'))
 f_numpy=array(ones((col,row),dtype='double'))
-#ShowPlot(u_numpy)
-%timeit Instant_result=solver_instant(u_numpy,f_numpy,col,row,t0=0,t1=1000,dt=.1,nu=1)
-#ShowPlot(Instant_result)
+ShowPlot(u_numpy)
+timeit Instant_result=solver_instant(u_numpy,f_numpy,col,row,t0=0,t1=1000,dt=.1,nu=1)
+ShowPlot(Instant_result)
