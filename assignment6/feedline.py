a04253763e3c657e9298289bef9b83d1bb95ce1d
Shayan Yazdanmehr <shayany@ifi.uio.no>
2015-11-16 11:30:36 +0100

    feedline has been created and tested

--- assignment6/feedline.py
+++ assignment6/feedline.py
@@ -1,16 +1,15 @@
 import sys
 from StringIO import StringIO
-namespace = vars().copy()
-code = """
-import random
-print "hello world"
-"""
-# Swap stdout with a StringIO instance
-oldio, sys.stdout = sys.stdout, StringIO()
-exec(code, namespace)
-# Get stdout buffer
-out = sys.stdout.getvalue()
-# Reset stdout
-sys.stdout = oldio
-# Print out captured stdout
-print out
+def feedline(code,namespace):
+    # Swap stdout with a StringIO instance
+    oldio, sys.stdout = sys.stdout, StringIO()
+    try:
+        eval(code, namespace)
+    except:
+        exec(code, namespace)        
+    # Get stdout buffer
+    out = sys.stdout.getvalue()
+    # Reset stdout
+    sys.stdout = oldio
+    # Print out captured stdout
+    return out
